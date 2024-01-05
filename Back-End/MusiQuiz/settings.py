b57b4e9f4b1950756393925d35a307543c955cb7
dvdmnc <130088298+dvdmnc@users.noreply.github.com>
2024-01-05 17:04:02 +0100

    Update settings.py

--- Back-End/MusiQuiz/settings.py
+++ Back-End/MusiQuiz/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'MusiQuiz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('NAME'),
         'USER': os.environ.get('USER'),
         'PASSWORD': os.environ.get('PASSWORD'),
