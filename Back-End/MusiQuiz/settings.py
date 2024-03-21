beb3383ef9e6153a6f5d474f7e14352aadd32a53
dvdmnc <130088298+dvdmnc@users.noreply.github.com>
2024-03-21 20:56:59 +0100

    Update settings.py

--- Back-End/MusiQuiz/settings.py
+++ Back-End/MusiQuiz/settings.py
@@ -99,6 +99,7 @@ SIMPLE_JWT = {
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -192,5 +193,5 @@ MEDIA_URL = '/media/'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'https://musiquizapp.up.railway.app'
+    'https://musiquizapp.up.railway.app/'
 ]
