a416c11164cd86db0671ed78c0efa4e6c6cb3633
dvdmnc <130088298+dvdmnc@users.noreply.github.com>
2024-03-21 21:30:35 +0100

    Update settings.py

--- Back-End/MusiQuiz/settings.py
+++ Back-End/MusiQuiz/settings.py
@@ -97,8 +97,8 @@ SIMPLE_JWT = {
 
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
